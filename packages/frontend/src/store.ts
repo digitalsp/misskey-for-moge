4432f84e6eb7f6d376e2c4f04ba40dd0fc3bae01
xFlest <mail@fle.st>
2023-02-13 00:01:05 +0900

    enhance: cjp button display settings

--- packages/frontend/src/store.ts
+++ packages/frontend/src/store.ts
@@ -278,6 +278,10 @@ export const defaultStore = markRaw(new Storage('base', {
 		where: 'device',
 		default: false,
 	},
+	cjpButton: {
+		where: 'device',
+		default: false,
+	},
 }));
 
 // TODO: 他のタブと永続化されたstateを同期
